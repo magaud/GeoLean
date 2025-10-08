e31fa5212e3363c3105f30246dea163c9a9b143f
magaud <magaud@unistra.fr>
2025-10-08 11:21:55 +0200

    making our first build of the project

--- GeoLean.lean
+++ GeoLean.lean
@@ -1,3 +1,9 @@
 -- This module serves as the root of the `GeoLean` library.
 -- Import modules here that should be built as part of the library.
 import GeoLean.Basic
+
+import GeoLean.theories.Axioms.beeson_s_axioms
+import GeoLean.theories.Axioms.makarios_variant_axioms
+import GeoLean.theories.Axioms.gupta_inspired_variant_axioms
+import GeoLean.theories.Coinc.Utils.general_tactics
+import GeoLean.theories.Axioms.tarski_axioms
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GeoLean
+
+A hand-made translation of the GeoCoq library into Lean.
+
+# technical details
+GeoLean.lean somehow plays the role of the _CoqProject file.
